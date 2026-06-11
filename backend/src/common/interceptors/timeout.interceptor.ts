f86b59833a6c4f342d36caa44072309b59bc0718
sondos.deeb37@gmail.com <sondos.deeb22@gmail.com>
2026-06-11 17:14:09 +0300

    feat(interceptor-timeout): build global timeout interceptor and insert it to main file

---
+++ backend/src/common/interceptors/timeout.interceptor.ts
@@ -0,0 +1,31 @@
+// =============================================================
+//? Import
+// =============================================================
+import {
+    Injectable,
+    NestInterceptor,
+    ExecutionContext,
+    CallHandler,
+    RequestTimeoutException,
+} from '@nestjs/common';
+import { Observable, TimeoutError } from 'rxjs';
+import { catchError, timeout } from 'rxjs/operators';
+
+// =============================================================
+
+@Injectable()
+export class TimeoutInterceptor implements NestInterceptor {
+    constructor(private readonly timeoutMs: number) { }
+
+    intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
+        return next.handle().pipe(
+            timeout(this.timeoutMs),
+            catchError((err) => {
+                if (err instanceof TimeoutError) {
+                    throw new RequestTimeoutException('Request timed out');
+                }
+                throw err;
+            }),
+        );
+    }
+}
--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,18 +1,23 @@
 // =============================================================
-//? Import 
+//? Import
 // =============================================================
 
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
+//interceptors
+import { TimeoutInterceptor } from './common/interceptors/timeout.interceptor';
+
 // =============================================================
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-
-
+  app.enableCors({
+    origin: true,
+    credentials: true,
+  });
   // --------------------------------------------
   const config = new DocumentBuilder()
     .setTitle('APIs Documentation for EasyLearn Web App')
@@ -24,9 +29,14 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('/apis', app, document);
 
-  await app.listen(process.env.PORT ?? 3000);
-}
+  app.useGlobalInterceptors(new TimeoutInterceptor(5 * 60 * 1000));
 
+
+
+
+  await app.listen(process.env.PORT ?? 3000);
+
+}
 
 // =============================================================
 bootstrap();
